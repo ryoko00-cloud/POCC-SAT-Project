bd8683165876e25fe2ef977ecfefc8ec877eaa64
Ryoko <ryoko.putra@student.citrakasih.sch.id>
2026-05-30 00:26:24 +0800

    plsssss

--- api/search.js
+++ api/search.js
@@ -4,7 +4,7 @@ export default async function handler (req, res) {
       ...req.query,
       api_key: process.env.SERPAPI_KEY,
     });
-    const response = await fetch('https://serpapi.com/search?${query}');
+    const response = await fetch(`https://serpapi.com/search?${query}`);
     const data = await response.json();
     res.json(data);
   } catch (err) {
